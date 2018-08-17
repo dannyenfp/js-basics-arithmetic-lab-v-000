70ae3be9728f6f2d522ae1414dc07b97a746c9e5
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-17 05:59:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.random()*20; 
 
-const randomNumber = Math.floor(randomNumber);
+const randomInteger = Math.floor(randomNumber);
 
 const randomUserID = 1000000000 + randomInteger;
